807e222afef5a2b67ae0063fb0a6e8b696d70662
Sarah Oliveira Souza <88009975+sarahfofiss@users.noreply.github.com>
2021-11-30 17:19:11 -0300

    Update Animal.cs

--- Animal.cs
+++ Animal.cs
@@ -1,8 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data;
 
 namespace Projetinho
 {
@@ -20,6 +22,21 @@ namespace Projetinho
 
         public void cadastrarPet()
         {
+            MySqlConnection conexao;
+            conexao = new MySqlConnection("server=localhost;database=petshop;uid=root;password=vertrigo");
+            try
+            {
+                conexao.Open();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message.ToString());
+                Console.WriteLine("Não foi possivel conectar-se ao banco");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+            MySqlCommand cmd;           
+
             Console.WriteLine("Digite o nome do pet");
             nomepet = Console.ReadLine();
             Console.WriteLine("Digite o tipo de animal");
@@ -39,29 +56,19 @@ namespace Projetinho
                 Console.WriteLine("Qual é a alergia?");
                 alergia = Console.ReadLine();
             }
-        }
 
-        /*public void atualizarPet()
-        {
-            Console.WriteLine("--- Recadastro de Pet ---");
-            Console.WriteLine("Digite o nome do pet");
-            nomepet = Console.ReadLine();
-            Console.WriteLine("Digite o tipo de animal");
-            tipo = Console.ReadLine();
-            Console.WriteLine("Digite a raça");
-            raca = Console.ReadLine();
-            Console.WriteLine("Digite a idade");
-            idade = double.Parse(Console.ReadLine());
-            Console.WriteLine("Digite o peso");
-            peso = double.Parse(Console.ReadLine());
-            Console.WriteLine("Digite a altura");
-            altura = double.Parse(Console.ReadLine());
-            Console.WriteLine("Possui alguma alergia? (s/n)");
-            alergia = Console.ReadLine();
-            if (alergia.ToLower().Equals("s"))
-            {
-                Console.WriteLine("Qual é a alergia?");
-                alergia = Console.ReadLine();
-            }*/
+            string sql;
+            sql = "insert into animal values(@nome_pet, @tipo, @raca, @idade, @peso, @altura, @alergia, @id_pet)";
+            cmd = new MySqlCommand(sql, conexao);
+            cmd.Parameters.AddWithValue("@nome_pet", nomepet);
+            cmd.Parameters.AddWithValue("@tipo", tipo);
+            cmd.Parameters.AddWithValue("@raca", raca);
+            cmd.Parameters.AddWithValue("@idade", idade);
+            cmd.Parameters.AddWithValue("@peso", peso);
+            cmd.Parameters.AddWithValue("@altura", altura);
+            cmd.Parameters.AddWithValue("@alergia", alergia);
+            cmd.Parameters.AddWithValue("@id_pet", id_pet);
+            cmd.ExecuteNonQuery();
+        }
     }
 }
