e220754dc614aeda1e04da428d375975eacc853f
Sarah Oliveira Souza <88009975+sarahfofiss@users.noreply.github.com>
2021-11-30 17:18:38 -0300

    Update Cliente.cs

--- Cliente.cs
+++ Cliente.cs
@@ -1,8 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data;
 
 namespace Projetinho
 {
@@ -17,6 +19,21 @@ namespace Projetinho
 
         public void cadastrarCliente()
         {
+            MySqlConnection conexao;
+            conexao = new MySqlConnection("server=localhost;database=petshop;uid=root;password=vertrigo");
+            try
+            {
+                conexao.Open();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message.ToString());
+                Console.WriteLine("Não foi possivel conectar-se ao banco");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+            MySqlCommand cmd;
+
             Console.WriteLine("Digite o CPF");
             cpf = double.Parse(Console.ReadLine());
             Console.WriteLine("Digite seu nome");
@@ -29,10 +46,36 @@ namespace Projetinho
             telefone = double.Parse(Console.ReadLine());
             Console.WriteLine("Digite o email");
             email = Console.ReadLine();
+
+            string sql;
+            sql = "insert into cliente values(@nome, @cpf, @cep, @pet, @telefone, @email)";
+            cmd = new MySqlCommand(sql, conexao);
+            cmd.Parameters.AddWithValue("@nome", nome);
+            cmd.Parameters.AddWithValue("@cpf", cpf);
+            cmd.Parameters.AddWithValue("@cep", cep);
+            cmd.Parameters.AddWithValue("@pet", pet);
+            cmd.Parameters.AddWithValue("@telefone", telefone);
+            cmd.Parameters.AddWithValue("@email", email);
+            cmd.ExecuteNonQuery();
         }
 
         public void atualizarCliente()
         {
+            MySqlConnection conexao;
+            conexao = new MySqlConnection("server=localhost;database=petshop;uid=root;password=vertrigo");
+            try
+            {
+                conexao.Open();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message.ToString());
+                Console.WriteLine("Não foi possivel conectar-se ao banco");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+            MySqlCommand cmd;
+
             Console.WriteLine("--- Recadastro de Cliente ---");
             Console.WriteLine("Digite o CPF");
             cpf = double.Parse(Console.ReadLine());
@@ -46,6 +89,17 @@ namespace Projetinho
             telefone = double.Parse(Console.ReadLine());
             Console.WriteLine("Digite o email");
             email = Console.ReadLine();
+
+            string sql;
+            sql = "update cliente set nome=@nome, cep=@cep, pet=@pet, telefone=@telefone, email=@email where cpf=@cpf";
+            cmd = new MySqlCommand(sql, conexao);
+            cmd.Parameters.AddWithValue("@cpf", cpf);
+            cmd.Parameters.AddWithValue("@nome", nome);
+            cmd.Parameters.AddWithValue("@cep", cep);
+            cmd.Parameters.AddWithValue("@pet", pet);
+            cmd.Parameters.AddWithValue("@telefone", telefone);
+            cmd.Parameters.AddWithValue("@email", email);
+            cmd.ExecuteNonQuery();
         }
     }
 }
